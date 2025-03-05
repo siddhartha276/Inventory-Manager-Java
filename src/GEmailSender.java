afe3d8abb02f45e9c5aecc046b80a8a667784273
Siddhartha Dhurve <94587311+siddhartha276@users.noreply.github.com>
2025-03-05 09:51:31 +0530

    Add files via upload

---
+++ src/GEmailSender.java
@@ -0,0 +1,54 @@
+
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class GEmailSender {
+
+    public boolean sendEmail(String to, String from, String subject, String text) {
+        boolean flag = false;
+
+        //logic
+        //smtp properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.starttls.enable", true);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+
+        String username = "siddhartha2706";
+        String password = "ltkqymbmdmdhmprk";
+
+
+        //session
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setFrom(new InternetAddress(from));
+            message.setSubject(subject);
+            message.setText(text);
+            Transport.send(message);
+            flag = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return flag;
+    }
+
+}
